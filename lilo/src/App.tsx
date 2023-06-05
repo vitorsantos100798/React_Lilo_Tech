822a5cd3329e061a3170f515f82513c2756217fe
Vitor Santos <githubvitor@gmail.com>
2023-06-05 02:03:42 -0300

    finished

--- lilo/src/App.tsx
+++ lilo/src/App.tsx
@@ -5,14 +5,24 @@ import { Main } from "./screens/main";
 import { Services } from "./screens/services";
 import { CardAbout } from "./screens/cardAbout";
 import { Contact } from "./screens/contact";
+import { Element } from "react-scroll";
+
 const App = () => {
   return (
     <div className="Background">
       <Header />
-      <Main />
-      <Services />
-      <CardAbout />
-      <Contact />
+      <Element name="main">
+        <Main />
+      </Element>
+      <Element name="services">
+        <Services />
+      </Element>
+      <Element name="about">
+        <CardAbout />
+      </Element>
+      <Element name="contact">
+        <Contact />
+      </Element>
     </div>
   );
 };
--- lilo/src/screens/header/index.tsx
+++ lilo/src/screens/header/index.tsx
@@ -1,20 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+import { Link } from "react-scroll";
+
 export const Header = () => {
   const logo = require("../../assets/logo.png");
+
+  const [selectedLink, setSelectedLink] = useState("home");
+
+  const handleLinkClick = (link: any) => {
+    setSelectedLink(link);
+  };
+
   return (
-    <>
-      <div className="container">
-        <div className="containerImg">
-          <img style={{ width: 150, height: 130 }} src={logo} />
-        </div>
-        <div className="containerList">
-          <p>Home</p>
-          <p>Serviços</p>
-          <p>Sobre</p>
-          <p>Contatos</p>
-        </div>
+    <div className="container">
+      <div className="containerImg">
+        <img style={{ width: 150, height: 130 }} src={logo} alt="Logo" />
       </div>
-    </>
+      <div className="containerList">
+        <Link
+          style={{
+            color: selectedLink === "home" ? "#0061A5" : "white",
+            fontFamily: "Arial",
+            fontWeight: "bolder",
+            fontSize: "16px",
+          }}
+          to="main"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          onClick={() => handleLinkClick("home")}
+        >
+          Home
+        </Link>
+        <Link
+          style={{
+            color: selectedLink === "services" ? "#0061A5" : "white",
+            fontFamily: "Arial",
+            fontWeight: "bolder",
+            fontSize: "16px",
+          }}
+          to="services"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          onClick={() => handleLinkClick("services")}
+        >
+          Serviços
+        </Link>
+        <Link
+          style={{
+            color: selectedLink === "about" ? "#0061A5" : "white",
+            fontFamily: "Arial",
+            fontWeight: "bolder",
+            fontSize: "16px",
+          }}
+          to="about"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          onClick={() => handleLinkClick("about")}
+        >
+          Sobre
+        </Link>
+        <Link
+          style={{
+            color: selectedLink === "contact" ? "#0061A5" : "white",
+            fontFamily: "Arial",
+            fontWeight: "bolder",
+            fontSize: "16px",
+          }}
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          onClick={() => handleLinkClick("contact")}
+        >
+          Contatos
+        </Link>
+      </div>
+    </div>
   );
 };
