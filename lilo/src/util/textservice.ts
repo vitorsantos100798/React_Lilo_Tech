682dd981a9eba74046b27ea5a60e20fb53dddc1b
Vitor Santos <githubvitor@gmail.com>
2023-06-07 10:10:01 -0300

    fineshed of change at page

--- lilo/src/util/textservice.ts
+++ lilo/src/util/textservice.ts
@@ -2,18 +2,23 @@ export const textService = [
   {
     textServiceOne:
       "Implementação e Integração de Sistemas: Auxilia na seleção, implementação e integração de sistemas de TI, como sistemas de gestão empresarial (ERP), sistemas de CRM (Customer Relationship Management), sistemas de segurança, entre outros.",
-    textServiceOneIntern: "Servico Interno 1",
+    textServiceOneIntern:
+      "O serviço de Suporte Técnico em TI é a solução ideal para empresas que desejam maximizar sua eficiência e manter sua infraestrutura tecnológica funcionando de forma impecável. Contamos com uma equipe de especialistas altamente qualificados, prontos para lidar com qualquer desafio técnico que possa surgir.",
     textServiceTwo:
       "Segurança da Informação: Avalia a segurança dos sistemas de TI, desenvolve políticas e procedimentos de segurança, implementa medidas de proteção e treina os funcionários para garantir a proteção dos dados e a conformidade com regulamentações.",
-    textServiceTwoIntern: "Serviço interno 2",
+    textServiceTwoIntern:
+      "Nosso serviço de Treinamento de Equipe é a solução ideal para impulsionar o desenvolvimento e a produtividade dos seus colaboradores, Nossos programas de treinamento são cuidadosamente elaborados para atender às necessidades específicas da sua empresa.Através de abordagens personalizadas, combinamos teoria e prática para proporcionar um treinamento envolvente e eficaz.",
     textServiceThree:
       "Análise de Dados e Business Intelligence: Auxilia sua empresas na coleta, análise e interpretação de dados para tomar decisões mais embasadas e estratégicas.",
-    textServiceThreeIntern: "Serviço Interno 3",
+    textServiceThreeIntern:
+      "Maximize o desempenho da sua rede e infraestrutura com nosso serviço especializado! Como especialistas em redes e infraestrutura, oferecemos soluções personalizadas para atender às suas necessidades específicas. Nossa equipe experiente irá analisar sua infraestrutura atual, identificar possíveis gargalos e implementar melhorias para otimizar o desempenho da sua rede.",
     textServiceFour:
       "Avaliação e Planejamento: Realiza uma análise abrangente dos sistemas existentes, processos de negócio e infraestrutura de TI para identificar áreas de melhoria e desenvolver um plano estratégico para implementar mudanças em sua empresa.",
-    textServiceFourIntern: "Serviço Interno 4",
+    textServiceFourIntern:
+      "Aproveite ao máximo a tecnologia com nossa Consultoria em TI! Somos especialistas em oferecer soluções personalizadas para impulsionar seu negócio. Nossa equipe experiente está pronta para analisar sua infraestrutura, identificar pontos de melhoria e implementar as melhores práticas. Seja aumentando a segurança cibernética, aprimorando a eficiência dos sistemas ou adotando tecnologias inovadoras, estamos aqui para ajudar.",
     textServiceFive:
       "Proteja sua empresa contra ameaças cibernéticas com nosso serviço de Teste de Intrusão. Nossos especialistas em segurança avaliarão sua infraestrutura, identificando vulnerabilidades e simulando ataques para garantir a robustez dos seus sistemas. ",
-    textServiceFiveIntern: "Serviço Interno 5",
+    textServiceFiveIntern:
+      "Modernize sua comunicação empresarial com nosso serviço de Telefonia IP! Nossa solução avançada utiliza a tecnologia de Voz sobre Protocolo de Internet para oferecer comunicações claras e confiáveis, ao mesmo tempo em que reduz seus custos de telefonia.",
   },
 ];
